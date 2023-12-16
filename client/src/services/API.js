06de46861a0b3d8dd6bf9c5050af69bcb5a00935
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2023-12-16 20:51:16 +0530

    server link added

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"http://localhost:8080/api/v1"});
+const API = axios.create({ baseURL:"https://bloodinventoryapp.onrender.com/api/v1"});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
