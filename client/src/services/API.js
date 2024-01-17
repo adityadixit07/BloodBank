b3c041e0faf7515e2b51dac4a061fa8caf65b98d
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2024-01-17 23:38:54 +0530

    build

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"https://bloodinventoryapp.onrender.com/api/v1"});
+const API = axios.create({ baseURL:"/api/v1"});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
